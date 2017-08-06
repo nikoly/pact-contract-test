599404697a6b8a652f242485de2a906f16105000
Yuliya Brynzak <yuliya@relayr.io>
2017-08-06 20:43:33 +0200

    Refactor file names and code a bit

--- contract_tests/translate_service_contract.py
+++ contract_tests/translate_service_contract.py
@@ -3,40 +3,47 @@ import unittest
 import requests
 from pact import Consumer, Provider
 
-pact = Consumer('Frontend').has_pact_with(
-    Provider(
-        'Translation Service'), pact_dir='./pacts')
+pact = Consumer('Translator').has_pact_with(Provider('Translate Service'), pact_dir='./pacts')
 
 pact.start_service()
 atexit.register(pact.stop_service)
 
 
+class TranslateServiceContract(unittest.TestCase):
 
-class GetDeviceContract(unittest.TestCase):
+    mock_host="http://localhost:1234"
+
+    def _request_helper(self, path):
+        url = self.mock_host + path
+        return requests.get(url)
+
     def test_get_translation_existing(self):
+        path = '/translate/1'
         expected = {"en": "One", "ua": "Один"}
           
         (pact
          .given('translation for number one')
          .upon_receiving('a request to get translation for number one')
-         .with_request('get', '/translate/1')
+         .with_request('get', path)
          .will_respond_with(200, body=expected))
 
         with pact:
-          result = requests.get('http://localhost:1234/translate/1').json()
+          import pdb; pdb.set_trace()
+          result = self._request_helper(path).json()
         
         self.assertEqual(result["en"], expected["en"])
 
     def test_get_translation_not_existing(self):
+        path = '/translate/-1'
         expected = 404
 
         (pact
          .given('for number -1 doesn\'t exist')
          .upon_receiving('a request to get translation for number minus one')
-         .with_request('get', '/translate/-1')
+         .with_request('get', path)
          .will_respond_with(expected, body=''))
 
         with pact:
-          result = requests.get('http://localhost:1234/translate/-1')
+          result = self._request_helper(path)
 
         self.assertEqual(result.status_code, expected)
